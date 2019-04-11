c6db9fd6f1c308e1ae68c021a0214ca697ab2273
Joonghyun Lee <jhlee3141592@gmail.com>
2019-04-11 14:27:47 +0900

    add a script to start rabbitmq container

---
+++ rabbitmq/create.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RABBITMQ_CONTAINER_NAME=rabbitmq
+
+docker \
+    run \
+    --detach \
+    --volume $PWD/data:/data \
+    --name ${RABBITMQ_CONTAINER_NAME} \
+    --env RABBITMQ_DEFAULT_USER=openstack \
+    --env RABBITMQ_DEFAULT_PASS='123qwe!@#' \
+    --restart=unless-stopped \
+    --publish 5672:5672 \
+    --publish 15672:15672 \
+    rabbitmq:3.4-management
