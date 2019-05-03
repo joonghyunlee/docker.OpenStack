ad14f1148030d27a918ea6b7a1118b681c60c385
Joonghyun Lee <jhlee3141592@gmail.com>
2019-05-03 11:44:30 +0900

    modify for rocky release

--- keystone/bootstrap.sh
+++ keystone/bootstrap.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
 # init arguments
-ADMIN_TOKEN=${ADMIN_TOKEN:-`openssl rand -hex 10`}
 ADMIN_TENANT_NAME=${ADMIN_TENANT_NAME:-admin}
 ADMIN_USERNAME=${ADMIN_USERNAME:-admin}
 ADMIN_PASSWORD=${ADMIN_PASSWORD:-New1234!}
-
-OS_TOKEN=$ADMIN_TOKEN
-OS_URL=${OS_URL:-http://${HOSTNAME}:35357/v3}
+OS_REGION_NAME=${OS_REGION_NAME:-RegionOne}
+OS_URL=${OS_URL:-http://${HOSTNAME}:5000/v3}
 OS_IDENTITY_API_VERSION=3
+
+KEYSTONE_DB_HOST=${KEYSTONE_DB_HOST:openstack-mysql}
+KEYSTONE_DB_PASS=${KEYSTONE_DB_PASSWORD:-6HN6gbDNtFY}
 
 # configure Keystone Service
 openstack-config --set /etc/keystone/keystone.conf DEFAULT admin_token $ADMIN_TOKEN
 openstack-config --set /etc/keystone/keystone.conf DEFAULT use_stder True
-openstack-config --set /etc/keystone/keystone.conf database connection ${CONNECTION:-mysql+pymysql://keystone:6HN6gbDNtFY@openstack-mysql/keystone}
+openstack-config --set /etc/keystone/keystone.conf database connection ${CONNECTION:-mysql+pymysql://keystone:${KEYSTONE_DB_PASS}@${KEYSTONE_DB_HOST}/keystone}
 openstack-config --set /etc/keystone/keystone.conf token provider fernet
 cat /etc/keystone/keystone.conf
 
-# initialize Fernet keys
+# bootstrap Keystone Service
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
 keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
 mv /etc/keystone/default_catalog.templates /etc/keystone/default_catalog
 chown -R keystone:keystone /etc/keystone
 
 su keystone -s /bin/sh -c "keystone-manage db_sync"
+
+keystone-manage bootstrap --bootstrap-password ${ADMIN_PASSWORD} \
+  --bootstrap-admin-url http://controller:5000/v3/ \
+  --bootstrap-internal-url http://controller:5000/v3/ \
+  --bootstrap-public-url http://controller:5000/v3/ \
+  --bootstrap-region-id ${OS_REGION_NAME}
+
 /usr/bin/keystone-wsgi-admin --port 35357 &
 
 cat > /keystonerc <<EOF
@@ -33,26 +41,13 @@ export OS_USER_DOMAIN_NAME=default
 export OS_PROJECT_NAME=${ADMIN_TENANT_NAME}
 export OS_USERNAME=${ADMIN_USERNAME}
 export OS_PASSWORD=${ADMIN_PASSWORD}
-export OS_AUTH_URL=http://${HOSTNAME}:35357/v3
+export OS_REGION_NAME=${OS_REGION_NAME}
+export OS_AUTH_URL=http://${HOSTNAME}:5000/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2
 EOF
 
-source /keystonerc
-
-# initialize account
-openstack service create --name keystone identity
-openstack endpoint create --region RegionOne identity public http://${HOSTNAME}:5000/v3
-openstack endpoint create --region RegionOne identity internal http://${HOSTNAME}:5000/v3
-openstack endpoint create --region RegionOne identity admin http://${HOSTNAME}:5000/v3
-
-openstack domain create --description "Default Domain" default
-openstack project create --domain default --description "Admin Project" $ADMIN_TENANT_NAME
-openstack user create --domain default --password $ADMIN_PASSWORD $ADMIN_USERNAME
-
-openstack role create admin
-openstack role add --project $ADMIN_TENANT_NAME --user $ADMIN_USERNAME admin
-
+# Launch httpd for Keystone
 cp /usr/share/keystone/wsgi-keystone.conf /etc/httpd/conf.d/
 rm -rf /run/httpd/* /tmp/httpd*
 exec /usr/sbin/apachectl -DFOREGROUND
