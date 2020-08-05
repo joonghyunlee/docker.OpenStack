0c3db786acc5ed2e1a5aadbcaa7923be011517fd
Joonghyun Lee <jhlee3141592@gmail.com>
2020-08-05 15:52:32 +0900

    RabbitMQ 구동 명령어 수정

--- rabbitmq/create.sh
+++ rabbitmq/create.sh
@@ -8,8 +8,8 @@ docker \
     --volume $PWD/data:/data \
     --name ${RABBITMQ_CONTAINER_NAME} \
     --env RABBITMQ_DEFAULT_USER=openstack \
-    --env RABBITMQ_DEFAULT_PASS='123qwe!@#' \
+    --env RABBITMQ_DEFAULT_PASS='123qwe' \
     --restart=unless-stopped \
     --publish 5672:5672 \
     --publish 15672:15672 \
-    rabbitmq:3.4-management
+    rabbitmq:3.6-management
